4520a3f145ddb089d51e42d790cbc3357cb890c3
mbecker20 <becker.maxh@gmail.com>
2024-01-08 02:16:25 -0800

    reformat 2 tab spaces

--- src/lib.rs
+++ src/lib.rs
@@ -4,54 +4,54 @@ use anyhow::{anyhow, Context};
 pub use serror::Serror;
 
 pub fn serialize_error(e: anyhow::Error) -> String {
-    let fallback = format!("{e:#?}");
-    try_serialize_error(e).unwrap_or(fallback)
+  let fallback = format!("{e:#?}");
+  try_serialize_error(e).unwrap_or(fallback)
 }
 
 pub fn try_serialize_error(e: anyhow::Error) -> anyhow::Result<String> {
-    let serror: Serror = e.try_into()?;
-    let res = serde_json::to_string(&serror)?;
-    Ok(res)
+  let serror: Serror = e.try_into()?;
+  let res = serde_json::to_string(&serror)?;
+  Ok(res)
 }
 
 pub fn serialize_error_pretty(e: anyhow::Error) -> String {
-    let fallback = format!("{e:#?}");
-    try_serialize_error_pretty(e).unwrap_or(fallback)
+  let fallback = format!("{e:#?}");
+  try_serialize_error_pretty(e).unwrap_or(fallback)
 }
 
 pub fn try_serialize_error_pretty(e: anyhow::Error) -> anyhow::Result<String> {
-    let serror: Serror = e.try_into()?;
-    let res = serde_json::to_string_pretty(&serror)?;
-    Ok(res)
+  let serror: Serror = e.try_into()?;
+  let res = serde_json::to_string_pretty(&serror)?;
+  Ok(res)
 }
 
 pub fn deserialize_error(json: String) -> anyhow::Error {
-    serror_into_error(deserialize_serror(json))
+  serror_into_error(deserialize_serror(json))
 }
 
 pub fn deserialize_serror(json: String) -> Serror {
-    try_deserialize_serror(&json).unwrap_or(Serror {
-        error: json.clone(),
-        trace: vec![json],
-    })
+  try_deserialize_serror(&json).unwrap_or(Serror {
+    error: json.clone(),
+    trace: vec![json],
+  })
 }
 
 pub fn try_deserialize_serror(json: &str) -> anyhow::Result<Serror> {
-    serde_json::from_str(json).context("failed to deserialize string into Serror")
+  serde_json::from_str(json).context("failed to deserialize string into Serror")
 }
 
 fn serror_into_error(mut serror: Serror) -> anyhow::Error {
-    let first = serror.trace.pop().unwrap_or(String::from("no error msg"));
+  let first = serror.trace.pop().unwrap_or(String::from("no error msg"));
 
-    let mut e = anyhow!("{first}");
+  let mut e = anyhow!("{first}");
 
-    loop {
-        let msg = serror.trace.pop();
-        if msg.is_none() {
-            break;
-        }
-        e = e.context(msg.unwrap());
+  loop {
+    let msg = serror.trace.pop();
+    if msg.is_none() {
+      break;
     }
+    e = e.context(msg.unwrap());
+  }
 
-    e
+  e
 }
--- src/serror.rs
+++ src/serror.rs
@@ -1,35 +1,34 @@
 use anyhow::Context;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Serror {
-    pub error: String,
-    pub trace: Vec<String>,
+  pub error: String,
+  pub trace: Vec<String>,
 }
 
 impl TryFrom<anyhow::Error> for Serror {
-    type Error = anyhow::Error;
-    fn try_from(e: anyhow::Error) -> Result<Serror, anyhow::Error> {
-        let e = serde_error::Error::new(&*e);
-        let e = serde_json::to_string(&e).context("failed to serialize error")?;
-        let e: Map<String, Value> =
-            serde_json::from_str(&e).context("failed to deserialize error")?;
-        let mut trace = Vec::<String>::new();
-        collapse_error_into_trace(e, &mut trace);
-        let serror = Serror {
-            error: trace.get(0).cloned().unwrap_or_default(),
-            trace,
-        };
-        Ok(serror)
-    }
+  type Error = anyhow::Error;
+  fn try_from(e: anyhow::Error) -> Result<Serror, anyhow::Error> {
+    let e = serde_error::Error::new(&*e);
+    let e = serde_json::to_string(&e).context("failed to serialize error")?;
+    let e: Map<String, Value> = serde_json::from_str(&e).context("failed to deserialize error")?;
+    let mut trace = Vec::<String>::new();
+    collapse_error_into_trace(e, &mut trace);
+    let serror = Serror {
+      error: trace.first().cloned().unwrap_or_default(),
+      trace,
+    };
+    Ok(serror)
+  }
 }
 
 fn collapse_error_into_trace(mut e: Map<String, Value>, trace: &mut Vec<String>) {
-    if let Some(Value::String(description)) = e.remove("description") {
-        trace.push(description);
-    }
-    if let Some(Value::Object(e)) = e.remove("source") {
-        collapse_error_into_trace(e, trace)
-    }
+  if let Some(Value::String(description)) = e.remove("description") {
+    trace.push(description);
+  }
+  if let Some(Value::Object(e)) = e.remove("source") {
+    collapse_error_into_trace(e, trace)
+  }
 }
