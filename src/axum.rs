8d470d178fc2d6e21a033b3a08a067b326fadfdc
mbecker20 <becker.maxh@gmail.com>
2024-06-06 14:40:31 -0700

    0.4.0 support piping headers through to response

--- src/axum.rs
+++ src/axum.rs
@@ -1,9 +1,9 @@
 use axum::{
+  body::Body,
   extract::{rejection::JsonRejection, FromRequest},
-  http::StatusCode,
+  http::{header::IntoHeaderName, HeaderMap, HeaderValue, StatusCode},
   response::{IntoResponse, Response},
 };
-use axum_extra::{headers::ContentType, TypedHeader};
 use serde::Serialize;
 
 use crate::serialize_error;
@@ -13,17 +13,35 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// Intermediate error type which can be converted to from any error using `?`.
 /// The standard `impl From<E> for Error` will attach StatusCode::INTERNAL_SERVER_ERROR,
 /// so if an alternative StatusCode is desired, you should use `.status_code` ([AddStatusCode] or [AddStatusCodeError])
-/// to add the status before using `?`.
-pub struct Error(pub StatusCode, pub anyhow::Error);
+/// to add the status and `.header` ([AddHeader] or [AddHeaderError]) before using `?`.
+pub struct Error {
+  pub status: StatusCode,
+  pub headers: HeaderMap,
+  pub error: anyhow::Error,
+}
+
+impl Error {
+  pub fn header(mut self, name: impl IntoHeaderName, value: HeaderValue) -> Error {
+    self.headers.append(name, value);
+    self
+  }
+
+  pub fn headers(mut self, headers: HeaderMap) -> Error {
+    self.headers = headers;
+    self
+  }
+}
 
 impl IntoResponse for Error {
   fn into_response(self) -> Response {
-    (
-      self.0,
-      TypedHeader(ContentType::json()),
-      serialize_error(&self.1),
-    )
-      .into_response()
+    let mut response = Response::new(Body::new(serialize_error(&self.error)));
+    *response.status_mut() = self.status;
+
+    let headers = response.headers_mut();
+    headers.append("Content-Type", HeaderValue::from_static("application/json"));
+    headers.extend(self.headers);
+
+    response
   }
 }
 
@@ -32,7 +50,11 @@ where
   E: Into<anyhow::Error>,
 {
   fn from(err: E) -> Self {
-    Self(StatusCode::INTERNAL_SERVER_ERROR, err.into())
+    Self {
+      status: StatusCode::INTERNAL_SERVER_ERROR,
+      headers: Default::default(),
+      error: err.into(),
+    }
   }
 }
 
@@ -40,7 +62,11 @@ where
 /// and converting error into anyhow error.
 pub trait AddStatusCodeError: Into<anyhow::Error> {
   fn status_code(self, status_code: StatusCode) -> Error {
-    Error(status_code, self.into())
+    Error {
+      status: status_code,
+      headers: Default::default(),
+      error: self.into(),
+    }
   }
 }
 
@@ -63,6 +89,19 @@ where
 {
 }
 
+/// Convenience trait to add headers to a serror::Result directly.
+pub trait AddHeaders<T>: Into<std::result::Result<T, Error>> {
+  fn header(self, name: impl IntoHeaderName, value: HeaderValue) -> Result<T> {
+    self.into().map_err(|e| e.header(name, value))
+  }
+  
+  fn headers(self, headers: HeaderMap) -> Result<T> {
+    self.into().map_err(|e| e.headers(headers))
+  }
+}
+
+impl<R, T> AddHeaders<T> for R where R: Into<Result<T>> {}
+
 /// Wrapper for axum::Json that converts parsing error to serror::Error
 #[derive(FromRequest)]
 #[from_request(via(axum::Json), rejection(JsonError))]
@@ -79,7 +118,11 @@ pub struct JsonError(Error);
 /// Convert the JsonRejection into JsonError(serror::Error)
 impl From<JsonRejection> for JsonError {
   fn from(rejection: JsonRejection) -> Self {
-    Self(Error(rejection.status(), rejection.into()))
+    Self(Error {
+      status: rejection.status(),
+      headers: Default::default(),
+      error: rejection.into(),
+    })
   }
 }
 
